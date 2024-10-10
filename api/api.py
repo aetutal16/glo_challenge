6f99990cc4e95bbf56001c1bbc25619d6a9d0a77
aetutal16 <aetuttal@gmail.com>
2024-10-10 17:20:31 -0500

    Security and validations adjusted
    -The security was improved with the .env  variables.
    -Now the api can receive files for the 3 tables.
    -Aditional validations applied to avoid errors

--- api/api.py
+++ api/api.py
@@ -2,74 +2,126 @@ from flask import Flask, request, jsonify
 import pandas as pd
 import psycopg2
 from psycopg2 import sql
+import os
+from dotenv import load_dotenv
+import hashlib
+
+# Load environment variables from the .env file
+load_dotenv()
 
 app = Flask(__name__)
 
-# Connection to postgreSQL
+# Connection toPostgreSQL
 def connect_db():
     try:
-        #Ajustar las siguientes lineas después de las pruebas
         conn = psycopg2.connect(
-            dbname="globant_challenge",
-            user="postgres",
-            password="andres123",
-            host="localhost",
-            port="5432"
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
         )
         return conn
     except Exception as e:
-        print(f"Connection error: {e}")
+        print(f"Connection Error: {e}")
         return None
+
+# Function to calculate the hash for the file
+def calculate_file_hash(file):
+    file_content = file.read()
+    file.seek(0) 
+    return hashlib.sha256(file_content).hexdigest()
+
+# Function to check if the file was already updated using the hash
+def is_file_already_uploaded(file_hash):
+    conn = connect_db()
+    if conn:
+        cursor = conn.cursor()
+        query = "SELECT 1 FROM file_uploads WHERE file_hash = %s"
+        cursor.execute(query, (file_hash,))
+        result = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        return result is not None
+    return False
+
+# Function to save the hash into the file_uploads table
+def log_file_upload(file_name, file_hash):
+    conn = connect_db()
+    if conn:
+        cursor = conn.cursor()
+        query = "INSERT INTO file_uploads (file_name, file_hash) VALUES (%s, %s)"
+        cursor.execute(query, (file_name, file_hash))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+# Function to insert the data into the postgres table
+def insert_data(df, table_name, num_columns):
+    conn = connect_db()
+    if conn:
+        cursor = conn.cursor()
+
+        placeholders = ', '.join(['%s'] * num_columns)
+        query = sql.SQL(f"INSERT INTO {{}} VALUES ({placeholders})").format(sql.Identifier(table_name))
+        
+        try:
+            for _, row in df.iterrows():
+                cursor.execute(query, tuple(row))
+            conn.commit()
+        except Exception as e:
+            print(f"Failed to insert data: {e}")
+            conn.rollback()
+            return [False, e]
+        finally:
+            cursor.close()
+            conn.close()
+        return [True,'-']
+    return [False, "Couldn't stablish the connection"]
 
 # Route to check if the server is working
 @app.route('/', methods=['GET'])
 def home():
     return "Flask server is working"
 
-# Function to insert data in the postgreSQL
-def insert_data(df, table_name):
-    conn = connect_db()
-    if conn:
-        #Remove the following line after the testing
-        print("Connected!")
-        cursor = conn.cursor()
-        for _, row in df.iterrows():
-            query = sql.SQL("INSERT INTO {} VALUES (%s, %s)").format(sql.Identifier(table_name))
-            cursor.execute(query, tuple(row))
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return True
-    return False
+# Route to upload the csv file
+@app.route('/upload_csv/<table_name>', methods=['POST'])
 
-# Route to upload the csv using POST
-@app.route('/upload_csv', methods=['POST'])
-def upload_csv():
-
-    if 'file' not in request.files:
-        return jsonify({"error": "There is no file in the field file"}), 400
-
-    file = request.files['file']
+def upload_csv(table_name):
+    file = request.files.get('file')
     
-    if file.filename == '':
-            return jsonify({"error": "The file's name is empty"}), 400
-
     if not file:
-        return jsonify({"error": "No file uploaded"}), 400
+        return jsonify({"error": "No file was uploaded"}), 400
     
     try:
-
-        df = pd.read_csv(file)
-
-        # Check if the file contains 2 columns
-        if df.shape[1] != 2:
-            return jsonify({"error": "The csv file should contain exactly 2 columns"}), 400
-
-        # Insert data into the postgres table
-        if insert_data(df, 'test_departments'):
-            return jsonify({"message": "Data inserted correctly"}), 201
+    
+        file_hash = calculate_file_hash(file)
+        
+        if is_file_already_uploaded(file_hash):
+            return jsonify({"error": "The file has already been uploaded before"}), 400
+        
+        df = pd.read_csv(file, header = None)
+        
+        # Check the number of columns according to the table
+        if table_name == 'dim_departments':
+            expected_columns = 2
+        elif table_name == 'dim_jobs':
+            expected_columns = 2
+        elif table_name == 'fact_hired_employees':
+            expected_columns = 5
         else:
-            return jsonify({"error": "Failed to insert data"}), 500
+            return jsonify({"error": "Invalid table!"}), 400
+        
+        if df.shape[1] != expected_columns:
+            return jsonify({"error": f"The csv file should have {expected_columns} columns"}), 400
+        
+        if insert_data(df, table_name, expected_columns):
+            
+            log_file_upload(file.filename, file_hash)
+            return jsonify({"message": "Data inserted correctly!"}), 201
+        
+        else:
+            return jsonify({"error": "Error inserting data!"}), 500
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
