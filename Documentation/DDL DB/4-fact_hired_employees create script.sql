afc4918c840a57c1f97c147c8cff370f2bcbfd6a
aetutal16 <aetuttal@gmail.com>
2024-10-11 17:54:25 -0500

    PK adjusted for fact table
    The primary key for the fact table was adjusted to avoid errors

--- Documentation/DDL DB/4-fact_hired_employees create script.sql
+++ Documentation/DDL DB/4-fact_hired_employees create script.sql
@@ -4,12 +4,13 @@
 
 CREATE TABLE IF NOT EXISTS public.fact_hired_employees
 (
-    id integer,
+    id integer NOT NULL,
     name text COLLATE pg_catalog."default",
     datetime text COLLATE pg_catalog."default",
     department_id integer,
     job_id integer,
     creation_date timestamp with time zone DEFAULT now(),
+    CONSTRAINT fact_hired_employees_pkey PRIMARY KEY (id),
     CONSTRAINT fact_hired_employees_department_id_fkey FOREIGN KEY (department_id)
         REFERENCES public.dim_departments (id) MATCH SIMPLE
         ON UPDATE NO ACTION
